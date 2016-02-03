3f386fc0c4d0c40145f2daec0fc650739094927c
James Wright <james@jamesswright.co.uk>
2016-02-03 21:19:13 +0000

    removed sed due to curly braces being metacharacters. paddle logic, mangling, and source maps

--- index.js
+++ index.js
@@ -1,5 +1,48 @@
 'use strict';
 
+/* The JS1K guys have made life easier by providing
+ * the following shim:
+ * - a - a canvas element
+ * - b - the body
+ * - c - a's 2D context
+ * - g - a's 3D context */
+
+var PI = Math.PI;
+
+var Paddle = {
+	init: function () {
+		this.rotation = 0;
+		this.x = a.width / 2;
+		this.y = a.width / 2;
+		this.radius = 50;
+
+		a.addEventListener('mousemove', this.rotate.bind(this, e));
+		this.render();
+	},
+	
+	rotate: function (e) {
+		this.rotation = this.x - e.clientX / 50;
+		this.render();
+	},
+
+	render: function () {
+		c.clearRect(
+			this.x - this.radius,
+			this.y - this.radius,
+			this.x + this.radius,
+			this.y + this.radius
+		);
+
+		c.strokeStyle = 'black';
+		c.beginPath();
+		c.arc(this.x, this.y, this.radius, PI - this.rotation, (PI + PI) - this.rotation);
+		c.stroke();
+	}
+};
+
+Paddle.render = requestAnimationFrame(Paddle.render);
+Paddle.init();
+
 drawFire();
 
 function drawFire() {
--- tasks/build.sh
+++ tasks/build.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 
 out_dir=dist
+header_end_line=125
+footer_start_line=126
+html_length=155
 browser_bin=chromium-browser
+html_file=index.html
+script=index.js
+source_map=index.map.js
 
 if [ ! -e $out_dir ]
 then
@@ -10,10 +16,16 @@ else
 	rm -rf $out_dir/*
 fi
 
-cp index.html $out_dir
-uglifyjs index.js > $out_dir/index.min.js
-echo "Minified to $out_dir/index.min.js, but going to inject into index.html..."
+cp $html_file $out_dir
+uglifyjs $script --compress --screw-ie8 --mangle "sort,toplevel" --source-map $out_dir/$source_map --source-map-url $source_map --source-map-root $script > $out_dir/index.min.js
 
-sed "s/jwscripthook/$(cat $out_dir/index.min.js)/g" index.html > $out_dir/index.html
+# Can't use sed as curly braces are metacharacters :(
+echo "Minified to $out_dir/index.min.js, but going to inject into $html_file..."
+head --lines $header_end_line $html_file > $out_dir/$html_file
+cat $out_dir/index.min.js >> $out_dir/$html_file
+
+footer_lines=$(expr $html_length - $footer_start_line)
+tail --lines $footer_lines $html_file >> $out_dir/$html_file
+
 echo "Done! Opening with $browser_bin..."
-$browser_bin $out_dir/index.html
+$browser_bin $out_dir/$html_file

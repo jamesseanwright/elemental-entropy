516a61c1cd73e054d6b92e9568e5b5e0563e11a5
James Wright <james@jamesswright.co.uk>
2016-03-05 23:53:38 +0000

    removed createParticle entirely. The only function remaining is the game loop, containing all logic. 950 bytes

--- index.pre-min.js
+++ index.pre-min.js
@@ -19,45 +19,13 @@ var angle = 0;
 
 var particles = [];
 var lastGeneration = 0;
-var fills = {
-	player: '#00f',
-	other: ['#f60', '#088']
-};
-
-var createParticle = function (options) {
-	options = options || {};
-
-	var isRandomX = !(Math.random() + 0.5 | 0);
-	var radius = options.radius || 25;
-	var x = options.x || (isRandomX ? Math.random() * 800 : (!(Math.random() + 0.5 | 0) ? 0 - radius : 800 + radius));
-	var y = options.y || (isRandomX ? (!(Math.random() + 0.5 | 0) ? 0 - radius : 480 + radius) : (!(Math.random() + 0.5 | 0) ? (480 / 6) - Math.random() * (480 / 6) : 480 - ((480 / 6) - Math.random() * (480 / 6))));
-	var xSpeed = 5 * (((400) - x) / (400));
-	var ySpeed = (5 * (((240) - y) / (240))) * (240 / 400);
-
-	var particle = {
-		isPlayer: options.isPlayer,
-		x: x,
-		y: y,
-		xSpeed: options.isPlayer ? 0 : xSpeed,
-		ySpeed: options.isPlayer ? 0 : ySpeed,
-		radius: radius,
-		fill: options.isPlayer ? fills.player : fills.other[(Math.random() * fills.other.length) | 0], // bitshift floor
-	};
-
-	particles.push(particle);
-    return particle;
-};
+var fills = ['#f60', '#088'];
 
 a.onmousemove = function (e) {
 	if (e.clientX > 100 && e.clientX < 700) angle = (Math.PI * 2) * ((e.clientX - 800) / 800) + Math.PI;
 };
 
-var player = createParticle({
-	isPlayer: true,
-	radius: 45,
-	x: 400,
-	y: 240
-});
+var player = true;
 
 var loop = function (ts) {
 	c.fillStyle = '#000';
@@ -65,8 +33,34 @@ var loop = function (ts) {
 
 	// Particle.next
 	if (ts - lastGeneration >= 1500 - (100 * level)) {
-		createParticle();
+		
+		var isRandomX = !(Math.random() + 0.5 | 0);
+		var x = (isRandomX ? Math.random() * 800 : (!(Math.random() + 0.5 | 0) ? 0 - 25 : 800 + 25));
+		var y = (isRandomX ? (!(Math.random() + 0.5 | 0) ? 0 - 25 : 480 + 25) : (!(Math.random() + 0.5 | 0) ? (480 / 6) - Math.random() * (480 / 6) : 480 - ((480 / 6) - Math.random() * (480 / 6))));
+		var xSpeed = 5 * (((400) - x) / (400));
+		var ySpeed = (5 * (((240) - y) / (240))) * (240 / 400);
+
+		var particle = {
+			x: x,
+			y: y,
+			xSpeed: xSpeed,
+			ySpeed: ySpeed,
+			fill: fills[(Math.random() * fills.length)|0], // bitshift floor
+		};
+
+		particles.push(particle);
+		
 		lastGeneration = ts;
+	}
+	
+	// render player
+	if (player) {
+		c.fillStyle = '#00f';
+		c.shadowColor = '#00f';
+		c.shadowBlur = 100;
+		c.beginPath();
+		c.arc(400, 240, 45, 0, Math.PI * 2);
+		c.fill();
 	}
 
 	for (var i in particles) {
@@ -76,46 +70,44 @@ var loop = function (ts) {
 			c.shadowColor = particles[i].fill;
 			c.shadowBlur = 100;
 			c.beginPath();
-			c.arc(particles[i].x, particles[i].y, particles[i].radius, 0, Math.PI * 2);
+			c.arc(particles[i].x, particles[i].y, 25, 0, Math.PI * 2);
 			c.fill();
 
-			if (particles[i] !== player) {
-				var oscillator;
+			var oscillator;
+			
+			// Particle.prototype.move
+			particles[i].x += particles[i].xSpeed;
+			particles[i].y += particles[i].ySpeed;
+	
+			//collider.detect (Particle)
+			if (player && (Math.sqrt((400 - particles[i].x) * (400 - particles[i].x) + (240 - particles[i].y) * (240 - particles[i].y)) < 45 + 25)) {
+				player = false;
 				
-				// Particle.prototype.move
-				particles[i].x += particles[i].xSpeed;
-				particles[i].y += particles[i].ySpeed;
-		
-				//collider.detect (Particle)
-				if (player && (Math.sqrt((player.x - particles[i].x) * (player.x - particles[i].x) + (player.y - particles[i].y) * (player.y - particles[i].y)) < player.radius + particles[i].radius)) {
-					player = particles[particles.indexOf(player)] = undefined;
-					
-					oscillator = ctx.createOscillator();
-					oscillator.frequency.value = 150;
-					oscillator.connect(ctx.destination);
-					oscillator.start(ctx.currentTime);
-					oscillator.stop(ctx.currentTime + 2);			
-				}
+				oscillator = ctx.createOscillator();
+				oscillator.frequency.value = 150;
+				oscillator.connect(ctx.destination);
+				oscillator.start(ctx.currentTime);
+				oscillator.stop(ctx.currentTime + 2);			
+			}
 
-				// collider.detect (shield)
-				if (player && !particles[i].isReversing && (Math.sqrt((400 - particles[i].x) * (400 - particles[i].x) + (240 - particles[i].y) * (240 - particles[i].y)) < 75 + particles[i].radius) && (Math.atan2(particles[i].y - (240), particles[i].x - (800 / 2)) >= (angle - Math.PI / 4) && Math.atan2(particles[i].y - (240), particles[i].x - (800 / 2)) <= (angle + Math.PI / 4))) {
-					particles[i].isReversing = true;
-					particles[i].xSpeed = particles[i].xSpeed * -1; // much easier than Math.abs and -() :D
-					particles[i].ySpeed = particles[i].ySpeed * -1;
+			// collider.detect (shield)
+			if (player && !particles[i].isReversing && (Math.sqrt((400 - particles[i].x) * (400 - particles[i].x) + (240 - particles[i].y) * (240 - particles[i].y)) < 75 + 25) && (Math.atan2(particles[i].y - (240), particles[i].x - (800 / 2)) >= (angle - Math.PI / 4) && Math.atan2(particles[i].y - (240), particles[i].x - (800 / 2)) <= (angle + Math.PI / 4))) {
+				particles[i].isReversing = true;
+				particles[i].xSpeed = particles[i].xSpeed * -1; // much easier than Math.abs and -() :D
+				particles[i].ySpeed = particles[i].ySpeed * -1;
 
-					score += 10;
-					if (score % 100 === 0) level++;
-					
-					oscillator = ctx.createOscillator();
-					oscillator.frequency.value = 190;
-					oscillator.connect(ctx.destination);
-					oscillator.start(ctx.currentTime);
-					oscillator.stop(ctx.currentTime + 0.1);
-				}
+				score += 10;
+				if (score % 100 === 0) level++;
 				
-				if (particles[i].x > 800 + particles[i].radius + 1 || particles[i].x < 0 - (particles[i].radius + 1) || particles[i].y > 480 + particles[i].radius + 1 || particles[i].y < 0 - (particles[i].radius + 1)) {
-					particles[i] = undefined;
-				}
+				oscillator = ctx.createOscillator();
+				oscillator.frequency.value = 190;
+				oscillator.connect(ctx.destination);
+				oscillator.start(ctx.currentTime);
+				oscillator.stop(ctx.currentTime + 0.1);
+			}
+			
+			if (particles[i].x > 800 + 25 + 1 || particles[i].x < 0 - (25 + 1) || particles[i].y > 480 + 25 + 1 || particles[i].y < 0 - (25 + 1)) {
+				particles[i] = undefined;
 			}
 		}
 	}
